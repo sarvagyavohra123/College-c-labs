635b752a601d8a2481cb23233bdc0abba8e6671c
sarvagyavohra123 <sarvagyavohra2020@gmail.com>
2026-08-30 12:49:10 +0530

    Add files via upload

---
+++ ADDITION.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main() {
+int a,b;
+clrscr();
+printf("enter first number:\n");
+scanf("%d", &a);
+printf("enter 2nd number:\n");
+scanf("%d", &b);
+printf("sum of 2 numbers is %d", a+b);
+getch();
+
+ }
